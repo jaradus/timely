7e0837b045942834615863fdb9c948146781f34b
Julie Kraft <julieschmoo@gmail.com>
2014-02-03 20:09:33 -0500

    index and site_view stuffs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
 
   def index
-		respond_to do |format|
-			format.html
+    # @sites = [{"name" => "name", "yelp_rating" => "yelp_rating", "address" => "address", "phone_number" => "phone_number"}]
+		# respond_to do |format|
+			# format.html { @sites }
 			#format.json {render json: Model.all.to_json}
-    end
+    # end
   end
 
 end
