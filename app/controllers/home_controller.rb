9d830f3ddc3872b60778a6dcd03ab3751af44de8
sergethompson <serge.thompson@gmail.com>
2014-02-03 20:57:22 -0500

    set defaults

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,9 +10,9 @@ class HomeController < ApplicationController
   def api_call
     latitude   = params[:latitude].to_f
     longitude  = params[:longitude].to_f
-    local_time = params[:local_time]
+    local_time = params[:local_time].to_i
 
-    results = lat_lon_keyword_search(latitude, longitude, ["bagels","coffee"])
+    results = lat_lon_keyword_search(latitude, longitude, check_time(local_time))
 
     results.each do |result|
       puts result.name
@@ -36,5 +36,26 @@ class HomeController < ApplicationController
 	def lat_lon_keyword_search(inLat, inLon, inKeywords)
 		return YelpApi.searchLatLonKeywords(inLat, inLon, inKeywords)
 	end
+
+end
+
+
+def check_time(local_time)
+	if local_time.between?(300,1130)
+		return ["breakfast", "bagel", "coffee"]
+	end
+	if local_time.between?(1130,1300)
+		return ["lunch", "burgers", "salads"]
+	end
+	if local_time.between?(1300,1730)
+		return ["coffee", "tea", "crumpets", "petit fours"]
+	end
+	if local_time.between?(1730,2000)
+		return ["dinner", "steak", "thai", "mexican"]
+	end
+	if 2000 < local_time || local_time < 30
+		return ["bars", "clubs", "dancing", "nightlife"]
+	end
+end
 
 end
