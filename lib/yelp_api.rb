8b93745b771587fbbaaef4293648c976d6a52bfa
Scooterlicious <sfell2006@gmail.com>
2014-02-04 17:01:45 -0500

    Fixed up the Yelp API and prettified it, along with adding the big stars and rearranging its functionality to be clearer

--- lib/yelp_api.rb
+++ lib/yelp_api.rb
@@ -1,18 +1,33 @@
+#########################################################
+# This class provides an interface to the Yelpster gem, which
+# provides the interface to the Yelp API.  This file exists
+# as a layer between the application and the Yelpster gem and
+# filters the application from having to know about how the
+# Yelp API returns its results.  
+#
+# Only the results relevant to the application are stored, 
+# and that data type is provided to the calling program
+#########################################################
 class YelpApi
+	#Constant that declares the maximum number of search
+	#results we get back from the Yelp API.  It's a good
+	#idea to limit this so our Yelp Keys don't get blocked
+	#bc of too much usage
+	MAX_RESULTS = 3
+
 	class YelpSite 
 		attr_accessor :cost, :name, :location, :city, :zipcode, :rating, :reviews, :yelp, :url, :image_url
 
-		def initialize(name, location, city, zip, rating, url, image_url, state_code)
-			@name = name
-			@location = location
-			@city = city
-			@zipcode = zip
-			@rating = rating
-			@yelp = true
-			@reviews = []
-			@url = url
-			@image_url = image_url
-			@state_code = state_code
+		def initialize(name, location, city, zip, state_code, rating, url, little_stars_url, big_stars_url)
+			@name 				= name
+			@location 			= location
+			@city 				= city
+			@zipcode 			= zip
+			@state_code 		= state_code
+			@rating 			= rating
+			@url 				= url
+			@little_stars_url 	= little_stars_url
+			@big_stars_url 		= big_stars_url
 		end
 	end
 
@@ -35,16 +50,18 @@ class YelpApi
 
 		include Yelp::V2::Search::Request
 
-		 # perform an address/location-based search for cream puffs nearby
+		# Set up the request information for a 
+		# lat/lon-based search
 		request_hash = GeoPoint.new(
-			:term => inKeywords.join(" "),
-			:latitude => inLat,
-		 	:longitude => inLon,
-            :limit => 3
+			:term 		=> inKeywords.join(" "),
+			:latitude 	=> inLat,
+		 	:longitude 	=> inLon,
+            :limit 		=> MAX_RESULTS
 	 	)		 
+
+		#################################
+	 	# Perform the actual search!
 	 	response = client.search(request_hash)
-#Scooter: Working on code here--commenting this out to commit
-#binding.pry 
 
 		site_array = []
 
@@ -64,29 +81,20 @@ class YelpApi
 		business_array.each do |business|
 			name 			= business["name"]
 			address 		= business["location"]["address"][0]
-
-
-#binding.pry 
-
-
 			city 			= business["location"]["city"]
 			zip 			= business["location"]["postal_code"]
-			url 			= business["url"]
-			image_url 		= business["rating_img_url_small"]
 			state_code		= business["location"]["state_code"]
-
-# binding.pry
-
-
-			#"reviews" key seems to be unavailable in V2
-			#results so I'm removing it
-			#review_array 	= business["reviews"]
-			#rating_sum 		= 0
-
-			rating = business["rating"]
+			rating 			= business["rating"]
+			url 			= business["url"]
+			little_stars_url= business["rating_img_url_small"]
+			big_stars_url	= business["rating_img_url_large"]
+			
+			#Create our own YelpSite data object with 
+			#JUST The data we're interested in
 			siteclassobject = YelpSite.new(
-				name, address, city, 
-				zip, rating, url, image_url, state_code
+				name, address, city, zip, state_code,
+				rating, 
+				url, little_stars_url, big_stars_url
 			)
 			
 			#Put the site object into the site array
@@ -94,76 +102,5 @@ class YelpApi
 		end
 		return site_array
 	end
-
-
-	#PLEASE BE AWARE this method has not been updated
-	# to work with all the changes necessary for the V2
-	# call.  Please do not assume that this call will
-	# work for you if you choose to use it for testing!
-	def self.searchZip(inZip)
-		#def yelp_test_query
-			Yelp.configure(
-				:yws_id          => ENV['YELP_YWSID'],
-				:consumer_key    => ENV['YELP_CONSUMER_KEY'],
-				:consumer_secret => ENV['YELP_CONSUMER_SECRET'],
-				:token           => ENV['YELP_TOKEN'],
-				:token_secret    => ENV['YELP_TOKEN_SECRET'])
-
-			 # construct a client instance
-			 client = Yelp::Client.new
-
-			 include Yelp::V1::Review::Request
-			 # perform an location/location-based search for cream puffs nearby
-
-			 request_hash = {
-			 # :location => '650 Mission St',
-	             #:city => 'New York',
-	             #:state => 'NY',
-	             :radius => 1,
-	             :zipcode => inZip,
-	             :term => 'restaurant',
-	             :business_count => 4,
-	             :compress_response => false
-	         }
-			 
-			 request = Location.new(request_hash)
-			 
-			site_array = []
-			puts "Calling request with these parameters: " + request_hash.to_s
-
-			response = client.search(request)
-
-			business_array = response["businesses"]
-
-			business_array.each do |business|
-				name = business["name"]
-				location = business["location"]
-				city = business["city"]
-				zip = business["zip"]
-				url = business["url"]
-
-
-				image_url = business["rating_img_url_small"]
-
-				review_array = business["reviews"]
-				rating_sum = 0
-
-				siteclassobject = YelpSite.new(
-					name, address, city, 
-					zip, 0, url, image_url)
-
-				review_array.each do |review|
-					rating_sum += review["rating"]
-					siteclassobject.reviews.push(
-						[review["rating"], 
-						review["text_excerpt"],
-						review["rating_img_url_small"]]
-						)
-				end
-				site_array << siteclassobject
-			end
-		return site_array
-
-	end #	def self.searchZip(inZip)
 
 end #class
