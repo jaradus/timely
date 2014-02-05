9b3f23086a03f253087a34d29aba6a8524bed6a8
Jaradus <ethanschmertzler@mac.com>
2014-02-04 21:57:29 -0500

    Tasks are put into a local memory array javascripts

--- app/assets/javascripts/preferences.js
+++ app/assets/javascripts/preferences.js
@@ -1,7 +1,9 @@
 var pref = {
 
   initialize: function(){
-    var keywords
+    pref.keywords
+    pref.local_keyword_repo = []
+
     pref.tabs = {
       // Tabs
       $morning    : $('#morningTab'),
@@ -43,10 +45,17 @@ var pref = {
       url: '/user_keywords',
       method: 'get',
       success: function(data){
-        pref.prefRender(data); 
-        console.log(data);
+        pref.prefRender(data);
       }
     });
+  },
+
+  // Keyword Constructor
+  Keyword: function(word, period_of_time, id){
+    var self = this;
+    this.keyword = word;
+    this.time_period = period_of_time;
+    this.id = id;
   },
 
   prefRender: function(data){
@@ -56,6 +65,10 @@ var pref = {
     // Renders morning keywords
     if (pref.keywords.morning.length > 0) {
       $.each(pref.keywords.morning,function(i,v){
+        // Creates a new Keyword object, and pushes it into the local_keyword_repo
+        item = new pref.Keyword(v.keyword,v.period_of_time,v.id);
+        pref.local_keyword_repo.push(item);
+
         pref.information.$morningContent.append(keywordContainer);
         pref.information.$morningContent.children().last().append(v['keyword']);
       });
@@ -63,6 +76,10 @@ var pref = {
     // Renders noon keywords
     if (pref.keywords.noon.length > 0) {
       $.each(pref.keywords.noon,function(i,v){
+        // Creates a new Keyword object, and pushes it into the local_keyword_repo
+        item = new pref.Keyword(v.keyword,v.period_of_time,v.id);
+        pref.local_keyword_repo.push(item);
+
         pref.information.$noonContent.append(keywordContainer);
         pref.information.$noonContent.children().last().append(v['keyword']);
       });
@@ -70,6 +87,10 @@ var pref = {
     // Renders afternoon keywords
     if (pref.keywords.afternoon.length > 0) {
       $.each(pref.keywords.afternoon,function(i,v){
+        // Creates a new Keyword object, and pushes it into the local_keyword_repo
+        item = new pref.Keyword(v.keyword,v.period_of_time,v.id);
+        pref.local_keyword_repo.push(item);
+
         pref.information.$afternoonContent.append(keywordContainer);
         pref.information.$afternoonContent.children().last().append(v['keyword']);
       });
@@ -77,6 +98,10 @@ var pref = {
     // Renders evening keywords
     if (pref.keywords.evening.length > 0) {
       $.each(pref.keywords.evening,function(i,v){
+        // Creates a new Keyword object, and pushes it into the local_keyword_repo
+        item = new pref.Keyword(v.keyword,v.period_of_time,v.id);
+        pref.local_keyword_repo.push(item);
+
         pref.information.$eveningContent.append(keywordContainer);
         pref.information.$eveningContent.children().last().append(v['keyword']);
       });
@@ -84,6 +109,10 @@ var pref = {
     // Renders night keywords
     if (pref.keywords.night.length > 0) {
       $.each(pref.keywords.night,function(i,v){
+        // Creates a new Keyword object, and pushes it into the local_keyword_repo
+        item = new pref.Keyword(v.keyword,v.period_of_time,v.id);
+        pref.local_keyword_repo.push(item);
+
         pref.information.$nightContent.append(keywordContainer);
         pref.information.$nightContent.children().last().append(v['keyword']);
       });
